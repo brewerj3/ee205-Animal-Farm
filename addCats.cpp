03edfef389b895bde1fcafa4ef4077671a39404c
brewerj3 <joshuabrewer784@gmail.com>
2022-04-01 11:26:06 -1000

    commented out old code

--- addCats.cpp
+++ addCats.cpp
@@ -19,36 +19,36 @@
 #include "config.h"
 #include "catValidation.h"
 
-int addCat( char nameToAdd[], enum Gender isGender, enum Breed isBreed, bool isFixedNew, Weight weightNew, enum Color collarColor1, enum Color collarColor2, unsigned long long license ){
-    if(currentNumberOfCats >= MAX_CATS){
-        fprintf( stderr, "%s: CurrentNumberOfCats exceeds maximum allowed cats in database.\n", PROGRAM_NAME);
-        return 0;
-    }
-    if( !isValidName(nameToAdd) ) {
-        fprintf( stderr, "%s: the name is not valid \n", PROGRAM_NAME);
-        return 0;
-    }
-    if( !isValidWeight( weightNew )) {
-        return 0;
-    }
-    else {
-        int index = 0;
-        while(catNameExists( index, nameToAdd )) { //looking to see if name already exists
-            index++;
-        }
-        index = 0; //reset index to look for empty location.
-        while(( checkForEmptyName( index ) ) && (MAX_CATS > index)){ //searching for first empty spot
-            index++;
-        }
-        strcpy(catsStruct[index].name, nameToAdd);
-        catsStruct[index].isFixed = isFixedNew;
-        catsStruct[index].gender = isGender;
-        catsStruct[index].breed = isBreed;
-        catsStruct[index].weight = weightNew;
-        catsStruct[index].color1 = collarColor1;
-        catsStruct[index].color2 = collarColor2;
-        catsStruct[index].license = license;
-        currentNumberOfCats++;
-        return index;
-    }
-}
+//int addCat( char nameToAdd[], enum Gender isGender, enum Breed isBreed, bool isFixedNew, Weight weightNew, enum Color collarColor1, enum Color collarColor2, unsigned long long license ){
+//    if(currentNumberOfCats >= MAX_CATS){
+//        fprintf( stderr, "%s: CurrentNumberOfCats exceeds maximum allowed cats in database.\n", PROGRAM_NAME);
+//        return 0;
+//    }
+//    if( !isValidName(nameToAdd) ) {
+//        fprintf( stderr, "%s: the name is not valid \n", PROGRAM_NAME);
+//        return 0;
+//    }
+//    if( !isValidWeight( weightNew )) {
+//        return 0;
+//    }
+//    else {
+//        int index = 0;
+//        while(catNameExists( index, nameToAdd )) { //looking to see if name already exists
+//            index++;
+//        }
+//        index = 0; //reset index to look for empty location.
+//        while(( checkForEmptyName( index ) ) && (MAX_CATS > index)){ //searching for first empty spot
+//            index++;
+//        }
+//        strcpy(catsStruct[index].name, nameToAdd);
+//        catsStruct[index].isFixed = isFixedNew;
+//        catsStruct[index].gender = isGender;
+//        catsStruct[index].breed = isBreed;
+//        catsStruct[index].weight = weightNew;
+//        catsStruct[index].color1 = collarColor1;
+//        catsStruct[index].color2 = collarColor2;
+//        catsStruct[index].license = license;
+//        currentNumberOfCats++;
+//        return index;
+//    }
+//}
