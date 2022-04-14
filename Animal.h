ff673ac780357366e36f67b702c2e8ce69a1d3a5
brewerj3 <joshuabrewer784@gmail.com>
2022-04-14 11:08:55 -1000

    started on animal class

---
+++ Animal.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  animal_farm_3 - EE 205 - Spr 2022
+///
+/// @file Animal.h
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date   14_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef ANIMAL_FARM_3_ANIMAL_H
+#define ANIMAL_FARM_3_ANIMAL_H
+
+#endif //ANIMAL_FARM_3_ANIMAL_H
