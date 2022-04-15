9b59fff712ef537470e545d6b170023368ecfd20
brewerj3 <joshuabrewer784@gmail.com>
2022-04-15 12:12:24 -1000

    re added node.h

--- Node.h
+++ Node.h
@@ -2,11 +2,15 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  animal_farm_3 - EE 205 - Spr 2022
 ///
-/// @file List.h
+/// @file Node.h
 /// @version 1.0
 ///
 /// @author Joshua Brewer <brewerj3@hawaii.edu>
 /// @date   15_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "Node.h"
+#pragma once
+
+#include <iostream>
+
+#include "config.h"
