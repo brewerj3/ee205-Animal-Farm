8c1df42843ac718d3f71ac50068b0badf96e6346
brewerj3 <joshuabrewer784@gmail.com>
2022-04-15 12:48:29 -1000

    redid Class Node

--- Node.h
+++ Node.h
@@ -14,3 +14,37 @@
 #include <iostream>
 
 #include "config.h"
+
+class Node{
+protected:
+    Node* next = nullptr;
+
+    static bool compareByAddress( const Node* node1, const Node* node2 ) {
+        if( node1 < node2 ) {
+            return false;
+        }
+        return true;
+    }
+
+public:
+    virtual void dump() const {
+        std::string className = "Node";
+        FORMAT( className, "This" ) << this << std::endl;
+        FORMAT( className, "Next pointer" ) << next << std::endl;
+    }
+    virtual bool validate() const noexcept {
+        if( next == nullptr ) {
+            return true;
+        }
+        //check for recursion
+        if( next == next->next ) {
+            std::cout << PROGRAM_NAME << ": Recursion!" << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    virtual bool operator>( const Node& rightSide ) {
+        return compareByAddress( this, &(Node&)rightSide );
+    }
+};
