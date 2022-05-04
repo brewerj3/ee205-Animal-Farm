561abcf078c14eaf75af2e7ed9fb5c3103d7239e
brewerj3 <joshuabrewer784@gmail.com>
2022-05-04 11:13:57 -1000

    added documentation for Doxygen in SinglyLinkedList.h

--- src/SinglyLinkedList.h
+++ src/SinglyLinkedList.h
@@ -14,16 +14,18 @@
 #include "Node.h"
 #include "List.h"
 
+/// A singlyLinkedList that inherits from List
 class SinglyLinkedList : public List {
 public:
     //Constructor
+    /// Make a SinglyLinkedList object
     SinglyLinkedList();
 
     //List handling
-    void push_front( Node* newNode );
-    Node* pop_front() noexcept override;
-    void insert_after( Node* currentNode, Node* newNode );
-    void dump() const noexcept override;
-    bool validate() const noexcept override;
+    void push_front( Node* newNode );                       ///< Insert a newNode to the front of the Linked List
+    Node* pop_front() noexcept override;                    ///< Remove the first node in the list and return it
+    void insert_after( Node* currentNode, Node* newNode );  ///< Insert a Node after the given Node
+    void dump() const noexcept override;                    ///< Dump the contents of the list
+    bool validate() const noexcept override;                ///< validate the Linked List
 
 };
