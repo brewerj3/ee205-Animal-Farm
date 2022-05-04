c91ffe8d9ef5e1a5aed035ce9028e6a4ca7d67a4
brewerj3 <joshuabrewer784@gmail.com>
2022-05-04 11:01:05 -1000

    added documentation for Doxygen in Cat.h

--- src/Cat.h
+++ src/Cat.h
@@ -15,25 +15,28 @@
 
 #include "Mammal.h"
 
+/// Cat inherits from Mammal and contains information about a Cat
 class Cat: public Mammal {
 protected:
-    std::string name;   // Name of the Cat
-    bool isFixed;       // Is the Cat fixed or not
+    std::string name;   ///< The name of the Cat
+    bool isFixed;       ///< Is the Cat fixed or not
 
 public:
     // Species Name
-    static const std::string SPECIES_NAME;
+    static const std::string SPECIES_NAME;      ///< The name of the Species
 
     // Max Weight of Cats
-    static const Weight::typeWeight MAX_WEIGHT;
+    static const Weight::typeWeight MAX_WEIGHT; ///< A Weight Class tha that holds information about the weight of the Cat
 
     // Constructors
+    /// Constructs a Cat with just a Name
     Cat( const std::string& newName ) : Mammal( MAX_WEIGHT, SPECIES_NAME ) {
         assert( validateName ( newName ) );
         name = newName;
         isFixed = false;
         Cat::validate();
     }
+    /// Creates a Cat with a Name, Color, Fixed state, Gender, Weight
     Cat( const std::string &newName, const Color newColor, const bool newIsFixed, const Gender newGender, const Weight::typeWeight newWeight
     ) : Mammal( newColor, newGender, newWeight, MAX_WEIGHT, SPECIES_NAME ) {
         assert( validateName( newName) );
@@ -43,19 +46,19 @@ public:
     }
 
     // Getters
-    std::string getName() const;
+    std::string getName() const;                        ///< Gets the name of the Cat
 
     // Setters
-    void setNameOfCat( const std::string& newName );
-    void fixCat() noexcept;
+    void setNameOfCat( const std::string& newName );    ///< Set the name of the Cat
+    void fixCat() noexcept;                             ///< Fixes the Cat(spay/neuter)
 
     // Print Method
-    void dump() const noexcept override;
+    void dump() const noexcept override;                ///< Dump the contents of the Cat Object
 
     // Make the cat say meow
-    std::string speak() const noexcept override;
+    std::string speak() const noexcept override;        ///< Make the cat Meow
 
     // Validation Methods
-    bool validate();
-    static bool validateName( const std::string& newName );
+    bool validate();                                            ///< Check if the object is Valid
+    static bool validateName( const std::string& newName );     ///< Check if the names is Valid
 };
