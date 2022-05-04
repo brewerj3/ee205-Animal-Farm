a608459a954578879ff04b7e9c319fa2715bfcca
brewerj3 <joshuabrewer784@gmail.com>
2022-05-04 10:38:25 -1000

    added documentaion for Doxygen in Mammal.h

--- src/Mammal.h
+++ src/Mammal.h
@@ -15,27 +15,29 @@
 
 #include "Color.h"
 #include "Animal.h"
-
+/// This class holds information to make a Mammal and inherits from Animal
 class Mammal: public Animal{
 protected:
-    Color color = Color::UNKNOWN_COLOR;
+    Color color = Color::UNKNOWN_COLOR; ///< The color of the Mammal
 
 public:
-    static const std::string MAMMAL_NAME;
+    static const std::string MAMMAL_NAME;   ///< The name of the Mammal
 
     //Constructors
+    /// Creates a simple Mammal
     Mammal( Weight::typeWeight newMaxWeight, const std::string &newSpecies ) : Animal( newMaxWeight, MAMMAL_NAME, newSpecies ) {};
+    /// Creates a complex Mammal
     Mammal( const Color newColor, const Gender newGender, const float newWeight, const float newMaxWeight, const std::string &newSpecies ) : Animal(newGender, newWeight, newMaxWeight, MAMMAL_NAME, newSpecies ) {
         setColor( newColor );
     };
 
     //Getters
-    Color getColor() const noexcept;
+    Color getColor() const noexcept;    ///< Get the Color
 
     //Setters
-    void setColor( Color newColor ) noexcept;
+    void setColor( Color newColor ) noexcept;   ///< Set the Color
 
     //Print out
-    void dump() const noexcept override;
+    void dump() const noexcept override;        ///< dump the contents of Mammal and Animal Class
 
 };
