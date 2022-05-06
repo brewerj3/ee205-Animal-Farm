49681d53f18ebbbe2ea33c80abe0c9d0f6050765
brewerj3 <joshuabrewer784@gmail.com>
2022-05-05 14:21:51 -1000

    fixed compiling errors

--- src/Cat.cpp
+++ src/Cat.cpp
@@ -2,7 +2,7 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab_13a_animal_farm_3 - EE 205 - Spr 2022
 ///
-/// @file cat.cpp
+/// @file Cat.cpp
 /// @version 3.0
 ///
 /// @author Joshua Brewer <brewerj3@hawaii.edu>
--- src/Cat.h
+++ src/Cat.h
@@ -2,7 +2,7 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab_13a_animal_farm_3 - EE 205 - Spr 2022
 ///
-/// @file cat.h
+/// @file Cat.h
 /// @version 3.0
 ///
 /// @author Joshua Brewer <brewerj3@hawaii.edu>
